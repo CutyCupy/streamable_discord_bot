c787732bb999f906ae0881fcf7a3b0a44a00ba75
CutyCupy <alex.ciupka@gmx.de>
2020-05-30 21:35:26 +0200

    Allowed hosts plattforms and command optimization

--- index.js
+++ index.js
@@ -1,51 +1,65 @@
 const botconfig = require("./botconfig.json");
 const Discord = require("discord.js");
 const axios = require('axios');
-const bot = new Discord.Client({disableEveryone: true});
+const bot = new Discord.Client({ disableEveryone: true });
 var fs = require('fs');
-bot.on("ready", async() => {
-    console.log(`Bot is online`)
-    if (help = botconfig.commands.help) {
-      bot.user.setActivity(`type ${botconfig.prefix + help.name} for help!`, "Custom")
-    } else {
-      bot.user.setAFK(true).then((user) => {
-        bot.user = user;
-      });
-    }
+bot.on("ready", async () => {
+  console.log(`Bot is online`)
+  if (help = botconfig.commands.help) {
+    bot.user.setActivity(`type ${botconfig.prefix + help.name} for help!`, "Custom")
+  } else {
+    bot.user.setAFK(true).then((user) => {
+      bot.user = user;
+    });
+  }
 });
 
 const commandMap = new Map([
   [botconfig.commands.help, (message) => {
     const cmds = []
-    for(const cmd of commandMap.keys()) {
+    for (const cmd of commandMap.keys()) {
       cmds.push(`${botconfig.prefix}${cmd.name}: ${cmd.description}`)
     }
-   message.channel.send(`Meine Commands:\n${cmds.join("\n")}`)
+    message.channel.send(`Meine Commands:\n${cmds.join("\n")}`)
   }],
-  [botconfig.commands.submit, (msg) => {
-    if (!(msg instanceof Message)) {
+  [botconfig.commands.submit, (message) => {
+    channel = message.channel
+    sender = message.author
+    data = fs.readFileSync('clips.json', 'utf8');
+    clips = JSON.parse(data);
+
+    var convMsg = message.content.split(" ");
+
+    if (convMsg.length <= 1) {
       return;
     }
-    channel = msg.channel
-    sender = msg.author
-    data = fs.readFileSync('clips.json', 'utf8'); 
-    clips = JSON.parse(data);
-    
-    var convMsg = message.content.split(" ");
-    
-    urlImport = urlImport + convMsg[1];
+
+    const video = convMsg[1]
+
+    var allowed = false
+    for (const host of botconfig.allowedHosts) {
+      if (video.startsWith(host)) {
+        allowed = true
+        break
+      }
+    }
+
+    if (!allowed) {
+      channel.send(`<@${sender.id}> <${video}> ist leider nicht von einer gültigen Website für Clips! \n Die gültigen Websites sind ${getAllowedHosts()}`)
+      return;
+    }
 
     for (const clip of clips) {
       if (clip.originalURL == urlImport) {
-        message.channel.send(`Dieser Clip wurde bereits von ${clip.submitter} eingereicht!`)
-        clip.submissions++; 
+        message.channel.send(`<@${sender.id}> Dieser Clip wurde bereits von ${clip.submitter} eingereicht!`)
+        clip.submissions++;
         json = JSON.stringify(clips);
         fs.writeFileSync('clips.json', json, 'utf8');
         return;
       }
     }
 
-    axios.get(urlImport,{
+    axios.get(urlImport + video, {
       auth: {
         username: uemail,
         password: upwd
@@ -53,14 +67,14 @@ const commandMap = new Map([
     }).then((resp) => {
       clips.push({
         submitter: message.author.username,
-        originalURL: urlImport,
+        originalURL: video,
         uploadCode: resp.data.shortcode,
         submissions: 1,
         ts: Date.now(),
       });
       json = JSON.stringify(clips); //convert it back to json
       fs.writeFileSync('clips.json', json, 'utf8');
-    },(error) => {
+    }, (error) => {
       console.log(error);
     });
   }]
@@ -74,15 +88,22 @@ var urlImport = "https://api.streamable.com/import?url=";
 var urlExport = "https://api.streamable.com/videos/"
 
 bot.on("message", async message => {
-  if(!message.content.startsWith(botconfig.prefix)) {
+  if (!message.content.startsWith(botconfig.prefix)) {
     return;
   }
   message.content = message.content.substring(1)
   const command = botconfig.commands[message.content.split(" ")[0]]
-  if(commandMap.has(command)) {
+  if (commandMap.has(command)) {
     commandMap.get(command)(message);
   }
-
 });
 
 bot.login(botconfig.token);
+
+function getAllowedHosts() {
+  var hosts = []
+  for (const host of botconfig.allowedHosts) {
+    hosts.push(`<${host}>`)
+  }
+  return hosts.join(", ")
+}
